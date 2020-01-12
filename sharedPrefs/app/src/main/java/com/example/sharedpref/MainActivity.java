d09e187bd442f0e3d95c54d50037bc3eb0df0940
Ruhul Amin <ruhul005@users.noreply.github.com>
2020-01-12 23:17:19 +0600

    main ew

--- sharedPrefs/app/src/main/java/com/example/sharedpref/MainActivity.java
+++ sharedPrefs/app/src/main/java/com/example/sharedpref/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sharedpref;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,12 @@ public class MainActivity extends AppCompatActivity {
     private TextView result;
     private Button saveButton;
     private EditText enterMessage;
+
+    //variables for shared prefs
+    private SharedPreferences myPrefs;
+    private static final String PREFS_NAME= "myPrefsFile";
+    // It actually create an xml file
+    //where data are being stored.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +35,29 @@ public class MainActivity extends AppCompatActivity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
+                //Putting Data into myPrefsFile
+                myPrefs = getSharedPreferences(PREFS_NAME,0);
+                SharedPreferences.Editor editor = myPrefs.edit(); //This allows us to edit
+
+                editor.putString("message",enterMessage.getText().toString());// getting message from text field
+                //Have to save all by commit
+
+                editor.commit();
+
+
             }
         });
+
+        //Getting data back and shoeing into Text View
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME,0);
+        if( prefs.contains("message")){
+            String message = prefs.getString("message","not Found");
+            result.setText("Message:"+ message);
+
+
+
+        }
 
 
     }
